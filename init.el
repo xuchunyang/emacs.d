dbdae5934641ed68b677ace0a9b09b5263455542
Chunyang Xu <xuchunyang56@gmail.com>
2015-05-20 00:04:06 +0800

    * init.el: Add more config for mu4e

--- init.el
+++ init.el
@@ -1171,16 +1171,49 @@ See also `describe-function-or-variable'."
   (setq mu4e-drafts-folder "/[Gmail].Drafts"
         mu4e-sent-folder   "/[Gmail].Sent Mail"
         mu4e-trash-folder  "/[Gmail].Trash")
+  ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+  (setq mu4e-sent-messages-behavior 'delete)
+  ;; skip duplicate messages (caused by the combination of Gmail and offlineimap)
+  (setq mu4e-headers-skip-duplicates t)
   ;; setup some handy shortcuts
   (setq mu4e-maildir-shortcuts
         '(("/INBOX"               . ?i)
-          ("/[Gmail].Sent Mail"   . ?s)
-          ("/[Gmail].Trash"       . ?t)))
+          ("/[Gmail].Sent Mail"   . ?s)))
   ;; allow for updating mail using 'U' in the main view:
   (setq mu4e-get-mail-command "proxychains4 offlineimap"
         mu4e-update-interval (* 15 60)  ; update every 15 minutes
         )
 
+  ;; show images
+  (setq mu4e-show-images t)
+
+  ;; use imagemagick, if available
+  (when (fboundp 'imagemagick-register-types)
+    (imagemagick-register-types))
+
+  ;; convert html emails properly
+  ;; Possible options:
+  ;;   - html2text -utf8 -width 72
+  ;;   - textutil -stdin -format html -convert txt -stdout
+  ;;   - html2markdown | grep -v '&nbsp_place_holder;' (Requires html2text pypi)
+  ;;   - w3m -dump -cols 80 -T text/html
+  ;;   - view in browser (provided below)
+  (setq mu4e-html2text-command "textutil -stdin -format html -convert txt -stdout")
+
+  ;; spell check
+  (add-hook 'mu4e-compose-mode-hook
+            (defun my-do-compose-stuff ()
+              "My settings for message composition."
+              (set-fill-column 72)
+              (flyspell-mode)))
+
+  ;; something about ourselves
+  (setq user-mail-address "xuchunyang56@gmail.com"
+        user-full-name  "Chunyang Xu"
+        mu4e-compose-signature
+        (concat
+         "Cheers,\n"
+         "Chunyang Xu\n"))
   ;; Send via msmtp (for socks proxy support)
   (setq message-sendmail-f-is-evil 't)
   (setq message-send-mail-function 'message-send-mail-with-sendmail)
