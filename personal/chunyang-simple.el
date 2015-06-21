cf5ddc2bccb26cb2dfa0db3a90ea1d62fa5284f5
Chunyang Xu <xuchunyang56@gmail.com>
2015-06-21 22:39:01 +0800

    Remove some uncommon function from *-simple.le

--- personal/chunyang-simple.el
+++ personal/chunyang-simple.el
@@ -62,57 +62,6 @@ With prefix argument, insert current date at point."
   (interactive "r")
   (message "%d" (count-lines beg end)))
 
-(defun imenu-elisp-sections ()
-  (setq imenu-prev-index-position-function nil)
-  (add-to-list 'imenu-generic-expression '("Sections" "^;;; \\(.+\\)$" 1) t))
-
-;; (add-hook 'emacs-lisp-mode-hook 'imenu-elisp-sections)
-
-(defun chunyang-mail-can-use-osx-open ()
-  "Return non-nil if the OS X \"open\" command is available for mailing."
-  (and (featurep 'ns)
-       (equal (executable-find "open") "/usr/bin/open")
-       (memq system-type '(darwin))))
-
-;;;###autoload
-(defun chunyang-mail-insert-to-mailer ()
-  "Send the message to your preferred mail client.
-This requires either the OS X \"open\" command, or the freedesktop
-\"xdg-email\" command to be available."
-  (interactive)
-  (save-excursion
-    ;; FIXME? use mail-fetch-field?
-    (let* ((to (progn
-		 (goto-char (point-min))
-		 ;; (forward-line)
-		 (and (looking-at "^To: \\(.*\\)")
-		      (match-string-no-properties 1))))
-	   (subject (progn
-		      (forward-line)
-		      (and (looking-at "^Subject: \\(.*\\)")
-			   (match-string-no-properties 1))))
-	   (body (progn
-		   (forward-line 2)
-		   (if (> (point-max) (point))
-		       (buffer-substring-no-properties (point) (point-max))))))
-      (if (and to subject body)
-	  (if (chunyang-mail-can-use-osx-open)
-	      (start-process "/usr/bin/open" nil "open"
-			     (concat "mailto:" to
-				     "?subject=" (url-hexify-string subject)
-				     "&body=" (url-hexify-string body)))
-	    (start-process "xdg-email" nil "xdg-email"
-			   "--subject" subject
-			   "--body" body
-			   (concat "mailto:" to)))
-        (error "Subject, To or body not found")))))
-
-(defun chunyang--notmuch-unread-count ()
-  "Number of unread mails by notmuch."
-  (string-to-number
-   (shell-command-to-string
-    "notmuch search tag:unread | wc -l")))
-
 
 ;;;###autoload
 (defun chunyang-make-another-scratch-buffer (arg)
@@ -149,20 +98,6 @@ prefix argument, the process's buffer is displayed."
 (defun chunyang-launch-sentinel (proc event)
   "Reports on changes in `chunyang-launch'ed applications."
   (message (format "%s: %s" proc event)))
-
-
-;;; TODO: finish this.
-(defun chunyang-kill-all-buffer ()
-  "Kill almost all buffers."
-  (interactive)
-  (mapc (lambda (elt)
-          (with-current-buffer elt
-            (save-buffer elt)))
-        (seq-remove
-         (lambda (elt)
-           (member (buffer-name elt)
-                   '("*scratch*" "*Messages*" "*Help*" "*info*")))
-         (buffer-list))))
 
 
 ;;; Download stuffs
@@ -257,19 +192,6 @@ See also Emacs SE question: URL
     #'+
     (mapcar #'string-to-number (split-string (buffer-substring beg end)))
     0)))
-
-;; Provide a keybinding to rapidly search through all files an entire git repo
-(defun git-grep-repo (search)
-  "git-grep the entire current repo"
-  (interactive
-   (progn
-     (require 'grep)
-     (list (grep-read-regexp))))
-  (let ((command (grep-expand-template
-                  (concat "git --no-pager grep --no-color -n -e <R> "
-                          "`git rev-parse --show-toplevel`") search)))
-    (grep-find command)))
-(global-set-key (kbd "C-x v f") 'git-grep-repo)
 
 
 ;;; Debug and Log
