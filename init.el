ebdfb9898ee1f001e583643a12f1de4096c6983c
Chunyang Xu <xuchunyang56@gmail.com>
2015-09-08 05:45:19 +0800

    Move all package.el config to init.el

--- init.el
+++ init.el
@@ -1,8 +1,10 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
 ;; I already put the following in my emacs-init.org, but `package.el' requires
 ;; mentioning it in `user-init-file'.
 (package-initialize)                    ; uncommented now because I need ELPA org
-(setq package-enable-at-startup t)      ; force to run `package-initialize'
-                                        ; again (for Emacs commit 066b26d)
 
 ;; `org-babel-load-file' is autoloaded, no need require anything
 (org-babel-load-file (locate-user-emacs-file "emacs-init.org"))
