9172566b2fa6f1cd6341ea3b2cca9ae7eba490b6
Chunyang Xu <xuchunyang56@gmail.com>
2015-09-07 21:02:16 +0800

    Update package.el setup

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 ;; I already put the following in my emacs-init.org, but `package.el' requires
 ;; mentioning it in `user-init-file'.
 (package-initialize)                    ; uncommented now because I need ELPA org
+(setq package-enable-at-startup t)      ; force to run `package-initialize'
+                                        ; again (for Emacs commit 066b26d)
 
 ;; `org-babel-load-file' is autoloaded, no need require anything
 (org-babel-load-file (locate-user-emacs-file "init.org"))
