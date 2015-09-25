57c74c4a12ee2bc0ae32e9178e050398b808619c
Chunyang Xu <xuchunyang56@gmail.com>
2015-09-25 18:23:27 +0800

    Add annotate-word.el

---
+++ lisp/annotate-word.el
@@ -0,0 +1,36 @@
+(defvar annotate-word--overlays-tail nil
+  "Hold overlays for tailing word explanations.")
+
+(defun annotate-word--overlay (str pt)
+  "Create an overlay with STR at PT."
+  (when (<= (1+ pt) (point-max))
+    (let* ((ol (make-overlay pt (1+ pt)))
+           (old-str (buffer-substring pt (1+ pt))))
+      (overlay-put ol 'display (concat str old-str))
+      (push ol annotate-word--overlays-tail))))
+
+(defvar annotate-word-alist
+  '(("sufficient" . "足够的")
+    ("primary" . "初级、主要")
+    ("corresponding" . "相应的")))
+
+(defun annotate-word ()
+  (interactive)
+  (setq prog-mode-p (derived-mode-p 'prog-mode))
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "\\w+" nil t)
+      (when (or (not prog-mode-p)
+                (and prog-mode-p (nth 4 (syntax-ppss))))
+        (let* ((word (match-string 0))
+               (pt (match-end 0))
+               (annotation (cdr (assoc word annotate-word-alist))))
+          (when annotation
+            (setq annotation (concat "(" annotation ")"))
+            (annotate-word--overlay annotation pt)))))))
+
+(defun annotate-word-clear ()
+  (interactive)
+  (mapc #'delete-overlay annotate-word--overlays-tail))
+
+(provide 'annotate-word)
