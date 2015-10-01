7d4942a42146a07f1521f44cf31e9f60343a4065
Chunyang Xu <xuchunyang56@gmail.com>
2015-10-01 10:23:46 +0800

    Use org-mode from Git

--- init.el
+++ init.el
@@ -1,10 +1,14 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 ;; I already put the following in my emacs-init.org, but `package.el' requires
 ;; mentioning it in `user-init-file'.
 (package-initialize)                    ; uncommented now because I need ELPA org
+
+;;; Install org from Git, since I have trouble to access http://orgmode.org/elpa/
+(add-to-list 'load-path "~/wip/org-mode/lisp")
+;; Various org-mode extensions
+(add-to-list 'load-path "~/wip/org-mode/contrib/lisp" :append)
 
 ;; `org-babel-load-file' is autoloaded, no need require anything
 (org-babel-load-file (locate-user-emacs-file "emacs-init.org"))
