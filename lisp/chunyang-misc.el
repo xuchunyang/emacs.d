4ed7765a617c071feb3de79f2a92fa1cb3a4b76a
Chunyang Xu <xuchunyang56@gmail.com>
2016-03-27 01:20:21 +0800

    Add comment

--- lisp/chunyang-misc.el
+++ lisp/chunyang-misc.el
@@ -1,5 +1,17 @@
 ;;; chunyang-misc.el --- Miscellaneous               -*- lexical-binding: t; -*-
 
+;;; Commentary:
+
+;;; Code:
+
+;; -----------------------------------------------------------------------------
+;; Show recent project/file in a buffer
+;;
+
+;; The code requires Projectile and Recentf for their data.  Also, this idea is
+;; inspired by Spacemacs's startup buffer, see <http://spacemacs.org/>.
+
+
 (defun chunyang-insert-file-list (list-display-name list)
   ;; Copied from `spacemacs-buffer//insert-file-list'.
   (when (car list)
@@ -25,7 +37,8 @@
     (insert "\n\n")
     (chunyang-insert-file-list "Projects:" projectile-known-projects)
     (goto-char (point-min))
-    (page-break-lines-mode))
+    (page-break-lines-mode)
+    (read-only-mode))
   (local-set-key (kbd "RET") 'widget-button-press)
   (local-set-key [down-mouse-1] 'widget-button-click))
 
