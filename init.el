4aa22e4da77a196810c22c551a1f22366c5dd155
Chunyang Xu <xuchunyang.me@gmail.com>
2016-08-07 10:42:50 +0800

    Remove workaround for a gnutls bug (bug#23225)

--- init.el
+++ init.el
@@ -5,14 +5,6 @@
 (setq debug-on-error t)
 (add-hook 'after-init-hook (lambda () (setq debug-on-error nil)))
 
-;; Workaround for [[notmuch:id:CAM-tV-9b9Qwh5sKddyXQ4ep4oooE==xqefCZ2dLyXOxtgvgKqA@mail.gmail.com][Email from Noam Postavsky: bug#23225: 25.1.50; url-retrie]]
-(when (and (eq system-type 'darwin)
-           (version< "25" emacs-version))
-  (advice-add 'gnutls-available-p :around #'ignore)
-  (setq gnutls-verify-error t)
-  (setq gnutls-trustfiles
-        (list "/usr/local/etc/openssl/cert.pem")))
-
 
 ;;; Start up
 
