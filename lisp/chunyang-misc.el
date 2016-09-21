c2de579229911a3f5e0bdda2ea39609bca60c5d6
Chunyang Xu <xuchunyang.me@gmail.com>
2016-09-22 01:31:50 +0800

    Add temp code samples in chunyang-misc
    they are not useful.

--- lisp/chunyang-misc.el
+++ lisp/chunyang-misc.el
@@ -170,6 +170,90 @@
   (interactive)
   (insert (key-description (read-key-sequence "Type the key: "))))
 
+(defun insert-command-on-key ()
+  (interactive)
+  (insert (format "%s" (key-binding (read-key-sequence "Type the key: ")))))
+
+;; (defun f (f a)
+;;   (let ((b 100))
+;;     (funcall f a b)))
+
+;; (f (lambda (x y) (+ y x)) 3)
+
+;; Functions compose
+(defmacro o (f g)
+  `(lambda (&rest r)
+     (apply ',f (list (apply ',g r)))))
+
+;; (seq-map 'window-width
+;;          (seq-filter (o minibufferp window-buffer) (window-list-1 nil t)))
+
+(defmacro o2 (f g &rest args)
+  `(apply ,f (list (apply ,g (list ,@args)))))
+
+;; also `apply-partially'
+;; (defalias '1+ (apply-partially '+ 1)
+;;   "Increment argument by one.")
+;;
+;; (1+ 10)
+;;   => 11
+
+
+
+;; Make local function definition
+;; FIXME: Use `flet' instead, though it is obsolete by `cl-flet'. (NO)
+;; FIXME: 用 `cl-letf' 支持 recursin
+(defmacro chunyang-flet (binding &rest body)
+  "Make one local function binding (allow recursin)."
+  (declare (indent 1))
+  (let* ((name (car binding))
+         (old-def (and (fboundp name) (symbol-function name)))
+         (args-and-body (cdr binding)))
+    `(prog2
+         (defun ,name ,@args-and-body)
+         ,@body
+       ,(if old-def
+            `(fset ',name ,old-def)
+          `(fmakunbound ',name)))))
+
+;; (defun factorial (n)
+;;   (chunyang-flet (aux
+;;                   (n acc)
+;;                   (if (< n 1) acc (aux (1- n) (* n acc))))
+;;     (aux n 1)))
+
+;; (factorial 3)
+
+
+;; Redirect shell command output to Emacs buffer
+
+(defun redirect-shell-output-setup ()
+  "Redirect shell command ouput to /tmp/emacs will appear in the buffer *Shell Command Ouput*."
+  (require 'filenotify)
+  (let ((buffer (get-buffer-create "*Shell Command Output*"))
+        (file "/tmp/emacs"))
+    ;; If file not exist, watcher won't start
+    (unless (file-exists-p file) (write-region 1 1 file))
+    (buffer-disable-undo buffer)
+    (file-notify-add-watch file '(change)
+                           (lambda (event)
+                             ;; (message "Event %S" event) ; Debug
+                             (when (eq 'changed (cadr event))
+                               (with-current-buffer buffer
+                                 (erase-buffer)
+                                 (insert-file-contents file)))))))
+;; (redirect-shell-output-setup)
+
+;; Launch isearch for other window/buffer
+(defun isearch-other-window ()
+  (interactive)
+  (let ((input (current-word)))
+    (other-window 1)
+    (isearch-forward-regexp nil t)
+    (and input (isearch-yank-string input))))
+
+(define-key ctl-x-4-map "s" #'isearch-other-window)
+
 
 (defun random-word ()
   "Pick a randome English word."
