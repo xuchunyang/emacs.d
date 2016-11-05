a6d6c5c61f46a6a35bf21f89ba895dde60a8be8f
Chunyang Xu <mail@xuchunyang.me>
2016-11-05 11:55:56 +0800

    Change from => to ⇒ for C-j in lisp-interaction-mode

--- lisp/chunyang-elisp.el
+++ lisp/chunyang-elisp.el
@@ -166,7 +166,7 @@
            (eval
             (eval-sexp-add-defvars (elisp--preceding-sexp)) lexical-binding)))
       (unless (current-line-empty-p) (terpri))
-      (princ "     => ")
+      (princ "     ⇒ ")
       (princ (with-temp-buffer
                (elisp--eval-last-sexp-print-value res t)
                (buffer-string))))
