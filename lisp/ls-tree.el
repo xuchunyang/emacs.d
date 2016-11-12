d5c00998acc24830b55b307c1f2e3bf9a1ae477f
Chunyang Xu <mail@xuchunyang.me>
2016-11-12 22:11:37 +0800

    * lisp/ls-tree.el: Fill package metadata

--- lisp/ls-tree.el
+++ lisp/ls-tree.el
@@ -1,4 +1,28 @@
-;; -*- lexical-binding: t; -*-
+;;; ls-tree.el --- List directory contents in a tree-like format like tree(1)  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Chunyang Xu
+
+;; Author: Chunyang Xu <mail@xuchunyang.me>
+;; Keywords: files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 (defun ls-tree--dir-files (dir)
   (let ((result nil))
@@ -43,3 +67,4 @@
   (ls-tree--intern 0 dir (ls-tree--dir-files dir)))
 
 (provide 'ls-tree)
+;;; ls-tree.el ends here
