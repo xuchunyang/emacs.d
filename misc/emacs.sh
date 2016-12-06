510b1921e9daeab18e4b3e7128473196ed31d17a
Chunyang Xu <mail@xuchunyang.me>
2016-12-06 21:40:12 +0800

    Merge {linux,mac}.sh into emacs.sh

---
+++ misc/emacs.sh
@@ -0,0 +1,44 @@
+# emacs.sh --- Terminal.app loves Emacs.app
+#
+# Terminal.app --> emacsclient(1) --> Emacs.app
+
+OS=$( uname -s )
+
+if [ "$OS" == "Darwin" ]; then
+    _open_emacs_window ()
+    {
+        open -a Emacs
+    }
+elif [ "$OS" == "Linux" ]; then
+    _open_emacs_window ()
+    {
+        # Activate Emacs GUI Window
+        xdotool windowactivate --sync $( xdotool search --class Emacs | tail -1 )
+    }
+else
+    _open_emacs_window ()
+    {
+        echo "Warning: Unsupport OS \"$OS\", do not know how to open Emacs.app"
+    }
+fi
+
+magit ()
+{
+    local repo=$1
+    (cd ${repo:=`pwd`} && emacsclient --eval '(magit-status)')
+    _open_emacs_window
+}
+
+info ()
+{
+    local node=$1
+    emacsclient --eval "(shell/info \"$node\")"
+    _open_emacs_window
+}
+
+find-file ()
+{
+    local file=$1
+    emacsclient --eval "(find-file \"$file\")"
+    _open_emacs_window
+}
