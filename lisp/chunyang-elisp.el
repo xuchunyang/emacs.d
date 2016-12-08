7d3545a77b52a48cd16c8f6b8bf41cad9e1c7cc4
Chunyang Xu <mail@xuchunyang.me>
2016-12-08 14:33:54 +0800

    Add button to remove advice in the Help buffer

--- lisp/chunyang-elisp.el
+++ lisp/chunyang-elisp.el
@@ -252,6 +252,34 @@
     (deactivate-mark)
     (command-execute key)))
 
+
+;;; Remove advice in the Help buffer
+
+(defun describe-function@advice-remove-button (&rest r)
+  "Add a button to remove advice."
+  (with-current-buffer "*Help*"
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "^:[-a-z]* advice: .\\(.*\\).$" nil t)
+        (let* ((fun (let ((s (buffer-string)))
+                      (with-temp-buffer
+                        (insert s)
+                        (delay-mode-hooks (emacs-lisp-mode))
+                        (goto-char 1)
+                        (save-match-data
+                          (symbol-at-point)))))
+               (advice (intern (match-string 1)))
+               (button-fun (lambda (_)
+                             (message "Removing %s from %s" advice fun)
+                             (advice-remove fun advice)
+                             (save-excursion (revert-buffer nil t))))
+               (inhibit-read-only t))
+          (insert " » ")
+          (insert-text-button "Remove" 'action button-fun 'follow-link t))))))
+
+(advice-add 'describe-function :after #'describe-function@advice-remove-button)
+(advice-add 'describe-symbol   :after #'describe-function@advice-remove-button)
+(advice-add 'describe-key      :after #'describe-function@advice-remove-button)
 
 (provide 'chunyang-elisp)
 
