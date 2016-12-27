d659a700c3bf4dceaedfbf15ca90a3327b7ab55d
Chunyang Xu <mail@xuchunyang.me>
2016-12-27 13:38:19 +0800

    Add two template files
    I want to work on these two idea someday.

---
+++ lisp/explain-crontab.el
@@ -0,0 +1,31 @@
+;;; explain-crontab.el --- Explain cron schedule expressions  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Chunyang Xu
+
+;; Author: Chunyang Xu <mail@xuchunyang.me>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Inspired by https://crontab.guru/
+;;
+;; TODO Do the actual work
+
+;;; Code:
+
+
+(provide 'explain-crontab)
+;;; explain-crontab.el ends here
---
+++ lisp/nian.el
@@ -0,0 +1,37 @@
+;;; nian.el --- A DateTime library                   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  Chunyang Xu
+
+;; Author: Chunyang Xu <mail@xuchunyang.me>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODOs:
+;; - TODO Try to write a DateTime library
+;;
+;; Ideas:
+;; - Use OOP in Emacs Lisp? See (info "(eieio) Top") for documentation.
+;;
+;; Links:
+;; - (info "(elisp) Time of Day")
+;; - https://www.kennethreitz.org/essays/introducing-maya-datetimes-for-humans
+;; - http://ruby-doc.org/stdlib-2.3.1/libdoc/date/rdoc/DateTime.html
+
+;;; Code:
+
+(provide 'nian)
+;;; nian.el ends here
