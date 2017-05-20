d2cdcb2d018146d4bd9714304ccac76581763434
Chunyang Xu <mail@xuchunyang.me>
2017-05-21 00:53:34 +0800

    * init.el (eww): Add chunyang-eww-import-bookmarks.

--- init.el
+++ init.el
@@ -2065,6 +2065,34 @@ This should be add to `find-file-hook'."
 
 (use-package eww
   :defer t
+  :preface
+  (defun chunyang-eww-import-bookmarks (bookmarks-html-file)
+    "Import bookmarks from BOOKMARKS-HTML-FILE."
+    (interactive "fBookmarks HTML File: ")
+
+    ;; Check if some libraries exist
+    (require 'eww)
+    (unless (require 'dom nil 'no-error)
+      (user-error "dom.el not available, it was added in Emacs 25.1"))
+    (unless (fboundp 'libxml-parse-html-region)
+      (user-error "`libxml-parse-html-region' not available, \
+your Emacs doesn't have libxml2 support"))
+
+    (with-temp-buffer
+      (insert-file-contents bookmarks-html-file)
+      (setq eww-bookmarks
+            (loop with dom = (libxml-parse-html-region (point-min) (point-max))
+                  for a in (dom-by-tag dom 'a)
+                  for url = (dom-attr a 'href)
+                  for title = (dom-text a)
+                  for time = (current-time-string
+                              (seconds-to-time
+                               (string-to-number
+                                (dom-attr a 'add_date))))
+                  collect (list :url url
+                                :title title
+                                :time time)))
+      (eww-write-bookmarks)))
   :config
   ;; XXX Both Google & DuckDuckGo are currently bocked in China
   (setq eww-search-prefix "https://duckduckgo.com/html/?q=")
