eb38563fe3cb731c9112ea1441673115e327e30c
Chunyang Xu <mail@xuchunyang.me>
2017-12-03 18:01:58 +0800

    * init.el (info-python): Python Documentation in Info format.

--- init.el
+++ init.el
@@ -3609,6 +3609,20 @@ provides similiar function."
   (setq python-shell-interpreter "python3")
   (setq python-indent-guess-indent-offset-verbose nil))
 
+;; Related projects:
+;;   - https://github.com/Wilfred/python-info
+;;   - https://bitbucket.org/jonwaltman/pydoc-info
+;;
+;; The Python 3 Documentation in texinfo format was downloaded from
+;; https://github.com/Wilfred/python-info/pull/12
+(use-package info-python
+  :about "Support C-h S in python-mode"
+  :homepage https://github.com/emacs-pe/info-python.el
+  :notes
+  :load-path "~/src/info-python.el"
+  :after python
+  :init (setq info-python-files '("python3")))
+
 
 ;;; OCaml
 
