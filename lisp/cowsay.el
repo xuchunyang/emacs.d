8b876008fbddab08ddb61533a459bde75989853b
Xu Chunyang <mail@xuchunyang.me>
2018-01-09 01:18:49 +0800

    http://www.gnu.org/licenses => https://www.gnu.org/licenses

--- lisp/cowsay.el
+++ lisp/cowsay.el
@@ -4,7 +4,7 @@
 
 ;; Author: Xu Chunyang <mail@xuchunyang.me>
 ;; Created: 2017-03-05
-;; Modified: 2018-01-04
+;; Modified: 2018-01-09
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
--- lisp/number-word.el
+++ lisp/number-word.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/xuchunyang/emacs.d
 ;; Keywords: number
 ;; Created: 2017-03-02
-;; Modified: 2018-01-04
+;; Modified: 2018-01-09
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
