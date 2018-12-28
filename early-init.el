a3f69271fc35b7802dd99c8c15fcf6f7941d6e5c
Xu Chunyang <mail@xuchunyang.me>
2018-12-28 14:20:43 +0800

    * early-init.el (package-archives): Use https.

--- early-init.el
+++ early-init.el
@@ -1,4 +1,4 @@
 (setq package-user-dir (concat "~/.emacs.d/elpa-" emacs-version))
-(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+(setq package-archives '(("gnu"   . "https://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "https://elpa.emacs-china.org/melpa/")))
 (setq package-quickstart t)
