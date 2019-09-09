f75f3e1c886871ad18daefd819592ecdfc2abe4c
Xu Chunyang <xuchunyang56@gmail.com>
2019-09-09 17:08:33 +0800

    * init.el: Load ~/.path even for macOS.

--- init.el
+++ init.el
@@ -148,7 +148,7 @@
 ;; [[https://emacs-china.org/t/topic/5507][Mac 下给 Emacs 设置 PATH 和 exec-path - Emacs-general - Emacs China]]
 ;; NOTE: When PATH is changed, run the following command
 ;; $ sh -c 'printf "%s" "$PATH"' > .path
-(when (and *is-mac* (not *is-mac-port*))
+(when *is-mac*
   (condition-case err
       (let ((path (with-temp-buffer
                     (insert-file-contents-literally "~/.path")
