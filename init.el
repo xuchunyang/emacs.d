cddc28f35195f0453770e4d45bafeab56d502788
Xu Chunyang <xuchunyang56@gmail.com>
2019-12-16 00:09:27 +0800

    * init.el: Use HTTPS of Tencent ELPA Mirror.
    Since HTTP is redirected to HTTPS anyway.

--- init.el
+++ init.el
@@ -34,8 +34,8 @@
                            ("melpa" . "https://elpa.emacs-china.org/melpa/")))
   (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                            ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
-  (setq package-archives '(("gnu"   . "http://mirrors.cloud.tencent.com/elpa/gnu/")
-                           ("melpa" . "http://mirrors.cloud.tencent.com/elpa/melpa/")))
+  (setq package-archives '(("gnu"   . "https://mirrors.cloud.tencent.com/elpa/gnu/")
+                           ("melpa" . "https://mirrors.cloud.tencent.com/elpa/melpa/")))
   (package-initialize))
 
 
