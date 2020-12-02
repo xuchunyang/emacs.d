c7e161125e8d4f2f15ceacd90e3c0aa77ff07bf3
Xu Chunyang <xuchunyang56@gmail.com>
2020-12-02 19:35:34 +0800

    * lisp/chunyang-org-capture-server.el: Update.

--- lisp/chunyang-org-capture-server.el
+++ lisp/chunyang-org-capture-server.el
@@ -8,13 +8,15 @@
 
 (defun chunyang-org-capture-server (request)
   (pcase-let (((eieio process headers) request))
-    ;; (message "%S" headers)
+    (message "%S" headers)
     (let ((path (assoc-default :GET headers)))
       ;; 只处理 GET /
       (if (not (and path (string= path "/")))
           (ws-send-404 process)
         (let ((href (assoc-default "href" headers))
               (title (assoc-default "title" headers)))
+          (if (not (and href title))
+              (ws-send-404 process "Missing href and/or title argument"))
           (setq title (decode-coding-string title 'utf-8))
           (push (list href title) org-stored-links)
           ;; XXX 不确定这对不对
