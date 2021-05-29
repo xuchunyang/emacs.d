65af838cfcde34f9f3ef6408ea0d146263198357
Xu Chunyang <xuchunyang56@gmail.com>
2021-05-29 08:53:27 +0800

    * early-init.el: Delete.

--- early-init.el
+++
@@ -1,4 +0,0 @@
-(setq package-user-dir (concat "~/.emacs.d/elpa-" emacs-version))
-(setq package-archives '(("gnu"   . "https://mirrors.cloud.tencent.com/elpa/gnu/")
-                         ("melpa" . "https://mirrors.cloud.tencent.com/elpa/melpa/")))
-(setq package-quickstart t)
